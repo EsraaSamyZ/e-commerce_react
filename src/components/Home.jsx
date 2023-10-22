5e59262ba3006e90f47be324bc1fda9908a92ee6
EsraaSamyZ <142722769+EsraaSamyZ@users.noreply.github.com>
2023-10-23 01:04:15 +0300

    updated Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,44 @@
-function Home() {
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const Home = () => {
+
+  const counterGlobalState = useSelector( store => store.counterReducer.counter );
+
   return (
-    <div>
-      <h2>Home Page</h2>
+    <div className="container mt-5">
+      <div className="jumbotron text-center">
+        <h2 className="display-4">Welcome to Our E-Commerce Store</h2>
+        <p className="lead h3">We have <span className='h2 text-success'>{counterGlobalState}</span> customers shopping with us today.</p>
+      </div>
+
+      <h3>Shop by Category</h3>
+      <ul className="list-group">
+        <li className="list-group-item">Electronics</li>
+        <li className="list-group-item">Clothing</li>
+        <li className="list-group-item">Home & Garden</li>
+      </ul>
+
+      <div className="card mt-4">
+        <div className="card-body">
+          <h5 className="card-title">Customer Testimonials</h5>
+          <p className="card-text">"This store has the best selection of products!" - John Doe</p>
+        </div>
+      </div>
+
+      <div className="row mt-4">
+        <div className="col-md-6">
+          <h3>Contact Us</h3>
+          <p>Email: info@example.com</p>
+          <p>Phone: +1 (123) 456-7890</p>
+        </div>
+        <div className="col-md-6">
+          <h3>Store Hours</h3>
+          <p>Monday - Friday: 9:00 AM - 8:00 PM</p>
+          <p>Saturday - Sunday: 10:00 AM - 6:00 PM</p>
+        </div>
+      </div>
+
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,50 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <Link className="navbar-brand" to="/">Logo</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/shop">Shop</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/counter">Counter</Link>
-            </li>
-          </ul>
+      <div className="container justify-content-between">
+        <div>
+          <Link className="navbar-brand" to="/">
+            Logo
+          </Link>
+        </div>
+        <div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/shop">
+                  Shop
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/counter">
+                  Counter
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
